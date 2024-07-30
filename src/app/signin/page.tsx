4c0df3d626190c2d27b3fcad20074c486fecae56
n-sakuma39 <naoto.sakuma39@gmail.com>
2024-07-30 21:48:26 +0900

    first commit

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -3,9 +3,9 @@ import { LoginForm } from "@/app/components/LoginForm";
 
 export default function SignInPage() {
   return (
-    <main className="flex flex-col items-center justify-start h-screen py-2 bg-gray-200">
-      <div className="w-full max-w-xl items-center justify-center mt-5">
-        <div className="w-full px-14 py-8 bg-white shadow-md rounded-lg">
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
+      <div className="w-full max-w-xl px-4">
+        <div className="w-full px-6 py-8 bg-white shadow-md rounded-lg">
           <LoginForm />
         </div>
       </div>
