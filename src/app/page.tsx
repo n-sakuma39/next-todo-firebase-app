2b32e55b57fb775b83b294b45e42af99ad3a3c4e
n-sakuma39 <naoto.sakuma39@gmail.com>
2024-08-01 23:46:38 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import TodoList from "@/components/Todo/userlist";
 import Header from "@/components/Header";
+import { getServerSession } from "next-auth/next";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/lib/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/signin");
+  }
+
+  if (session.user?.role === "admin") {
+    redirect("/dashboard");
+  }
   return (
     <>
       <Header />
